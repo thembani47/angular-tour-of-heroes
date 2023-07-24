3c79fce74a177afbd9a23705f753cd41e383dd40
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2023-07-24 14:12:22 +0200

    Update heroes.component.ts

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Thembani'
-  // }
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
